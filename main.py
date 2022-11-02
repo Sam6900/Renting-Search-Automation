a970817d831cc0a92b666058fd06928f5d368bb2
Sam6900 <85671637+Sam6900@users.noreply.github.com>
2022-11-02 23:01:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+req_headers = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'en-US,en;q=0.8',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+}
+
+response = requests.get("https://www.zillow.com/homes/for_rent/?searchQueryState=%7B%22usersSearchTerm%22%3A%22New%20York%2C%20NY%22%2C%22mapBounds%22%3A%7B%22west%22%3A-74.66679775790395%2C%22east%22%3A-72.97490322665395%2C%22south%22%3A39.99634482557994%2C%22north%22%3A41.09412251223541%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22price%22%3A%7B%22min%22%3A0%2C%22max%22%3A569389%7D%2C%22mp%22%3A%7B%22min%22%3A0%2C%22max%22%3A3000%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22ah%22%3A%7B%22value%22%3Atrue%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A9%7D",
+                        headers=req_headers)
+zillow_data = response.text
+
+soup = BeautifulSoup(zillow_data, "html.parser")
+prices_data = soup.select(".hRqIYX span")
+prices = []
+for price in prices_data:
+    price = price.get_text()
+    if "/" in price:
+        price = price.split("/")[0]
+    elif "+" in price:
+        price = price.split("+")[0]
+    prices.append(price)
+
+addresses = [addr.get_text() for addr in soup.select("a address")]
+print(addresses)
